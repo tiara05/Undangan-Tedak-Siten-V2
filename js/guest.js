b6f4a61806c511a404fdc97f0f1d66c3c7ec332b
Tiara Rahmania <tiararahmania05@gmail.com>
2025-10-16 14:19:07 +0700

    final

--- js/guest.js
+++ js/guest.js
@@ -11,23 +11,22 @@ export const guest = (() => {
                   hour = minute * 60,
                   day = hour * 24;
           
-            //I'm adding this section so I don't have to keep updating this pen every year :-)
-            //remove this if you don't need it
+
             let today = new Date(),
                 dd = String(today.getDate()).padStart(2, "0"),
                 mm = String(today.getMonth() + 1).padStart(2, "0"),
                 yyyy = today.getFullYear(),
                 nextYear = yyyy + 1,
-                dayMonth = "09/30/",
-                birthday = dayMonth + yyyy;
-            
-            today = mm + "/" + dd + "/" + yyyy;
+                month = 9, 
+                date = 25,
+                birthday = new Date(yyyy, month, date, 10, 0, 0); 
+
             if (today > birthday) {
-              birthday = dayMonth + nextYear;
+                birthday = new Date(nextYear, month, date, 10, 0, 0);
             }
-            //end
+
             
-            const countDown = new Date(birthday).getTime(),
+            const countDown = birthday.getTime(),
                 x = setInterval(function() {    
           
                   const now = new Date().getTime(),
